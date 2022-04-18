4e561e8c135180da31c4fba11c20dd31e616df91
JosiahJohns <57999307+JosiahJohns@users.noreply.github.com>
2022-04-17 21:58:00 -0500

    Create databaseUpdateForm.php

---
+++ databaseUpdateForm.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+	
+		<form action="databaseUpdate.php" method="get">
+    UPDATE PLAYER NAME<br>
+		Player ID: <input type="number" name="id"><br>
+		New First Name: <input type="text" name="first"><br>
+		<input type="submit">
+		</form>
+
+</body>
+</html>
