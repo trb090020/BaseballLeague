21c8602a34717bdd31906c7883a3b78770453abe
JosiahJohns <57999307+JosiahJohns@users.noreply.github.com>
2022-04-29 19:41:05 -0500

    Update PitchervsTeamStats.php

--- PitchervsTeamStats.php
+++ PitchervsTeamStats.php
@@ -12,13 +12,90 @@
 				$team = $_GET['team'];
 			?>
 			
-			<?php
-				$pitcherfname = pg_query($PGDB, "SELECT  first_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
-				$pitcherlname = pg_query($PGDB, "SELECT  last_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
+			<?php	
+				require_once 'config.php';
+
+							try 
+							{$dsn = "pgsql:host=$host;port=5432;dbname=$db;";} 
+							catch (PDOException $e) 
+							{die($e->getMessage());}
+							
+							$pitcherfname = pg_query($PGDB, "SELECT  first_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
+							$pitcherlname = pg_query($PGDB, "SELECT  last_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
+							
+							$result = pg_query($PGDB, "SELECT 
+											ROUND(AVG(strikes),2) AS Strikes,
+											ROUND(AVG(balls),2) AS Balls,
+											ROUND(AVG(bases),2) AS Bases,
+											COUNT(*) AS AtBatCount,
+											COUNT(case when abresult='H' then 1 end) as Hits,
+											COUNT(case when abresult='O' then 1 end) as StrikeOuts,
+											ROUND((COUNT(case when abresult='H' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS percentHits,
+											ROUND((COUNT(case when abresult='O' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS PercentStrikeOuts
+											FROM public.atbatdetails
+											WHERE public.atbatdetails.pid = '$id'
+											AND public.atbatdetails.batterteam = '$team' ");
+											
+							$result2 = pg_query($PGDB,  "SELECT 
+													ROUND(AVG(strikes),2) AS Strikes,
+													ROUND(AVG(balls),2) AS Balls,
+													ROUND(AVG(bases),2) AS Bases,
+													COUNT(*) AS AtBatCount,
+													COUNT(case when abresult='H' then 1 end) as Hits,
+													COUNT(case when abresult='O' then 1 end) as StrikeOuts,
+													ROUND((COUNT(case when abresult='H' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS percentHits,
+													ROUND((COUNT(case when abresult='O' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS PercentStrikeOuts
+													FROM public.atbatdetails
+													WHERE public.atbatdetails.pid = '$id' AND 
+													public.atbatdetails.batterteam ='$team' AND 
+													public.atbatdetails.gdate <= 
+													(SELECT DISTINCT public.atbatdetails.gdate AS gdates 
+													FROM public.atbatdetails 
+													WHERE public.atbatdetails.pid = '$id' AND public.atbatdetails.batterteam ='$team' 
+													ORDER BY public.atbatdetails.gdate 
+													LIMIT 1 
+													OFFSET 9");
+													
+							$result3 = pg_query($PGDB,  "SELECT 
+													ROUND(AVG(strikes),2) AS Strikes,
+													ROUND(AVG(balls),2) AS Balls,
+													ROUND(AVG(bases),2) AS Bases,
+													COUNT(*) AS AtBatCount,
+													COUNT(case when abresult='H' then 1 end) as Hits,
+													COUNT(case when abresult='O' then 1 end) as StrikeOuts,
+													ROUND((COUNT(case when abresult='H' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS percentHits,
+													ROUND((COUNT(case when abresult='O' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS PercentStrikeOuts
+													FROM public.atbatdetails
+													WHERE public.atbatdetails.pid = '$id' AND 
+													public.atbatdetails.batterteam ='$team' AND 
+													public.atbatdetails.gdate <= 
+													(SELECT DISTINCT public.atbatdetails.gdate AS gdates 
+													FROM public.atbatdetails 
+													WHERE public.atbatdetails.pid = '$id' AND public.atbatdetails.batterteam ='$team' 
+													ORDER BY public.atbatdetails.gdate 
+													LIMIT 1 
+													OFFSET 4");
+													
+						$result4 = pg_query($PGDB,  "SELECT 
+													public.atbatdetails.gdate as d,
+													public.atbatdetails.glocation as l,
+													public.atbatdetails.bid as bid,
+													public.atbatdetails.first_name as f,
+													public.atbatdetails.last_name as lst,
+													public.atbatdetails.batterteam as tm,
+													public.atbatdetails.strikes as strikes,
+													public.atbatdetails.balls as balls,
+													public.atbatdetails.bases as bases,
+													public.atbatdetails.abresult as abresult
+													FROM public.atbatdetails
+													WHERE public.atbatdetails.pid = '$id' AND 
+													public.atbatdetails.batterteam ='$team'
+													ORDER BY public.atbatdetails.gdate DESC");
+					
 			?>
 			<hr size="1" width="100%" color="black">
 
-			<p class="text"><b>All-Time Statistics:</b><?php &nbsp; echo $pitcherfname;&nbsp;echo $pitcherlname;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
+			<p class="text"><b>All-Time Statistics: </b><?php echo $pitcherfname." ".$pitcherlname." ";?><b>vs. </b> <?php echo $team;?></p>
 			<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -30,28 +107,7 @@
 							<th>Hit %</th>
 							<th>Strikeout %</th>
 						</tr>
-						<?php
-							require_once 'config.php';
 
-							try 
-							{$dsn = "pgsql:host=$host;port=5432;dbname=$db;";} 
-							catch (PDOException $e) 
-							{die($e->getMessage());}
-
-				
-						$result = pg_query($PGDB, "SELECT 
-											ROUND(AVG(strikes),2) AS Strikes,
-											ROUND(AVG(balls),2) AS Balls,
-											ROUND(AVG(bases),2) AS Bases,
-											COUNT(*) AS AtBatCount,
-											COUNT(case when abresult='H' then 1 end) as Hits,
-											COUNT(case when abresult='O' then 1 end) as StrikeOuts,
-											ROUND((COUNT(case when abresult='H' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS percentHits,
-											ROUND((COUNT(case when abresult='O' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS PercentStrikeOuts
-											FROM public.atbatdetails
-											WHERE public.atbatdetails.pid = '$id'
-											AND public.atbatdetails.batterteam = '$team';");
-					?>
 							<!-- PHP CODE TO FETCH DATA FROM ROWS-->
 					<?php 
 							while ($rows = pg_fetch_assoc($result))
@@ -76,7 +132,7 @@
 				
 			<hr size="1" width="100%" color="black">
 			<h2><b>Pitcher vs. Team Statistics (Last 10 Games)</b></h2><br>	
-			<p class="text"><b>Statistics For Last 10 Games:</b><?php &nbsp; echo $pitcherfname;&nbsp;echo $pitcherlname;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
+			<p class="text"><b>Statistics For Last 10 Games:</b><?php echo $pitcherfname." ".$pitcherlname." ";?><b>vs. </b> <?php echo $team;?></p>
 			<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -89,27 +145,6 @@
 							<th>Strikeout %</th>
 						</tr>
 						
-					<?php	
-						$result2 = pg_query($PGDB,  "SELECT 
-													ROUND(AVG(strikes),2) AS Strikes,
-													ROUND(AVG(balls),2) AS Balls,
-													ROUND(AVG(bases),2) AS Bases,
-													COUNT(*) AS AtBatCount,
-													COUNT(case when abresult='H' then 1 end) as Hits,
-													COUNT(case when abresult='O' then 1 end) as StrikeOuts,
-													ROUND((COUNT(case when abresult='H' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS percentHits,
-													ROUND((COUNT(case when abresult='O' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS PercentStrikeOuts
-													FROM public.atbatdetails
-													WHERE public.atbatdetails.pid = '$id' AND 
-													public.atbatdetails.batterteam ='$team' AND 
-													public.atbatdetails.gdate <= 
-													(SELECT DISTINCT public.atbatdetails.gdate AS gdates 
-													FROM public.atbatdetails 
-													WHERE public.atbatdetails.pid = '$id' AND public.atbatdetails.batterteam ='$team' 
-													ORDER BY public.atbatdetails.gdate 
-													LIMIT 1 
-													OFFSET 9");
-					?>
 							<!-- PHP CODE TO FETCH DATA FROM ROWS-->
 					<?php 
 							while ($rows2 = pg_fetch_assoc($result2))
@@ -134,7 +169,7 @@
 				
 				<hr size="1" width="100%" color="black">
 				<h2><b>Pitcher vs. Team Statistics (Last 5 Games)</b></h2><br>
-				<p class="text"><b>Statistics For Last 5 Games:</b><?php &nbsp; echo $pitcherfname;&nbsp;echo $pitcherlname;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
+				<p class="text"><b>Statistics For Last 5 Games:</b><?php echo $pitcherfname." ".$pitcherlname." ";?><b>vs. </b> <?php echo $team;?></p>
 				<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -147,27 +182,6 @@
 							<th>Strikeout %</th>
 						</tr>
 						
-					<?php	
-						$result3 = pg_query($PGDB,  "SELECT 
-													ROUND(AVG(strikes),2) AS Strikes,
-													ROUND(AVG(balls),2) AS Balls,
-													ROUND(AVG(bases),2) AS Bases,
-													COUNT(*) AS AtBatCount,
-													COUNT(case when abresult='H' then 1 end) as Hits,
-													COUNT(case when abresult='O' then 1 end) as StrikeOuts,
-													ROUND((COUNT(case when abresult='H' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS percentHits,
-													ROUND((COUNT(case when abresult='O' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS PercentStrikeOuts
-													FROM public.atbatdetails
-													WHERE public.atbatdetails.pid = '$id' AND 
-													public.atbatdetails.batterteam ='$team' AND 
-													public.atbatdetails.gdate <= 
-													(SELECT DISTINCT public.atbatdetails.gdate AS gdates 
-													FROM public.atbatdetails 
-													WHERE public.atbatdetails.pid = '$id' AND public.atbatdetails.batterteam ='$team' 
-													ORDER BY public.atbatdetails.gdate 
-													LIMIT 1 
-													OFFSET 4 ");
-					?>
 							<!-- PHP CODE TO FETCH DATA FROM ROWS-->
 					<?php 
 							while ($rows3 = pg_fetch_assoc($result3))
@@ -192,7 +206,7 @@
 				
 				<hr size="1" width="100%" color="black">
 				<h2><b>Pitcher vs. Team Statistics (All At Bat Records)</b></h2><br>
-				<p class="text"><b>All At Bat Records:</b><?php &nbsp; echo $pitcherfname;&nbsp;echo $pitcherlname;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
+				<p class="text"><b>All At Bat Records:</b><?php echo $pitcherfname." ".$pitcherlname." ";?><b>vs. </b> <?php echo $team;?></p>
 				<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -209,23 +223,6 @@
 							<th>Result</th>
 						</tr>
 						
-					<?php	
-						$result4 = pg_query($PGDB,  "SELECT 
-													public.atbatdetails.gdate as d,
-													public.atbatdetails.glocation as l,
-													public.atbatdetails.bid as bid,
-													public.atbatdetails.first_name as f,
-													public.atbatdetails.last_name as lst,
-													public.atbatdetails.batterteam as tm,
-													public.atbatdetails.strikes as strikes,
-													public.atbatdetails.balls as balls,
-													public.atbatdetails.bases as bases,
-													public.atbatdetails.abresult as abresult
-													FROM public.atbatdetails
-													WHERE public.atbatdetails.pid = '$id' AND 
-													public.atbatdetails.batterteam ='$team'
-													ORDER BY public.atbatdetails.gdate DESC");
-					?>
 							<!-- PHP CODE TO FETCH DATA FROM ROWS-->
 					<?php 
 							while ($rows4 = pg_fetch_assoc($result4))
