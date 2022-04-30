068624ce57cd7cd2494f5fd2ef353016a1a6fb08
JosiahJohns <57999307+JosiahJohns@users.noreply.github.com>
2022-04-29 19:17:15 -0500

    Update PitchervsTeamStats.php

--- PitchervsTeamStats.php
+++ PitchervsTeamStats.php
@@ -13,12 +13,12 @@
 			?>
 			
 			<?php
-				$pitchername = pg_query($PGDB, "SELECT  first_name, last_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
+				$pitcherfname = pg_query($PGDB, "SELECT  first_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
+				$pitcherlname = pg_query($PGDB, "SELECT  last_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
 			?>
-			<br><hr size="1" width="100%" color="black">
+			<hr size="1" width="100%" color="black">
 
-			<p class="text"><b>All-Time Statistics:</b><?php &nbsp; echo $pitchername;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
-			
+			<p class="text"><b>All-Time Statistics:</b><?php &nbsp; echo $pitcherfname;&nbsp;echo $pitcherlname;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
 			<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -30,8 +30,16 @@
 							<th>Hit %</th>
 							<th>Strikeout %</th>
 						</tr>
-						
-					<?php	$result = pg_query($PGDB, "SELECT 
+						<?php
+							require_once 'config.php';
+
+							try 
+							{$dsn = "pgsql:host=$host;port=5432;dbname=$db;";} 
+							catch (PDOException $e) 
+							{die($e->getMessage());}
+
+				
+						$result = pg_query($PGDB, "SELECT 
 											ROUND(AVG(strikes),2) AS Strikes,
 											ROUND(AVG(balls),2) AS Balls,
 											ROUND(AVG(bases),2) AS Bases,
@@ -42,7 +50,7 @@
 											ROUND((COUNT(case when abresult='O' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS PercentStrikeOuts
 											FROM atbatdetails
 											WHERE atbatdetails.pid = '$id'
-											AND atbatdetails.batterteam = '$team'");
+											AND atbatdetails.batterteam = '$team';");
 					?>
 							<!-- PHP CODE TO FETCH DATA FROM ROWS-->
 					<?php 
@@ -67,8 +75,8 @@
 			</section>
 				
 			<hr size="1" width="100%" color="black">
-				
-			<p class="text"><b>Statistics For Last 10 Games:</b><?php &nbsp; echo $pitchername;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
+			<h2><b>Pitcher vs. Team Statistics (Last 10 Games)</b></h2><br>	
+			<p class="text"><b>Statistics For Last 10 Games:</b><?php &nbsp; echo $pitcherfname;&nbsp;echo $pitcherlname;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
 			<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -100,7 +108,7 @@
 													WHERE atbatdetails.pid = '$id' AND atbatdetails.batterteam ='$team' 
 													ORDER BY atbatdetails.gdate 
 													LIMIT 1 
-													OFFSET 9 ");
+													OFFSET 9");
 					?>
 							<!-- PHP CODE TO FETCH DATA FROM ROWS-->
 					<?php 
@@ -125,8 +133,8 @@
 				</section>
 				
 				<hr size="1" width="100%" color="black">
-				
-				<p class="text"><b>Statistics For Last 5 Games:</b><?php &nbsp; echo $pitchername;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
+				<h2><b>Pitcher vs. Team Statistics (Last 5 Games)</b></h2><br>
+				<p class="text"><b>Statistics For Last 5 Games:</b><?php &nbsp; echo $pitcherfname;&nbsp;echo $pitcherlname;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
 				<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -158,7 +166,7 @@
 													WHERE atbatdetails.pid = '$id' AND atbatdetails.batterteam ='$team' 
 													ORDER BY atbatdetails.gdate 
 													LIMIT 1 
-													OFFSET 4");
+													OFFSET 4 ");
 					?>
 							<!-- PHP CODE TO FETCH DATA FROM ROWS-->
 					<?php 
@@ -183,8 +191,8 @@
 				</section>
 				
 				<hr size="1" width="100%" color="black">
-				
-				<p class="text"><b>All At Bat Records:</b><?php &nbsp; echo $pitchername;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
+				<h2><b>Pitcher vs. Team Statistics (All At Bat Records)</b></h2><br>
+				<p class="text"><b>All At Bat Records:</b><?php &nbsp; echo $pitcherfname;&nbsp;echo $pitcherlname;&nbsp;?><b>vs.</b> <?php &nbsp;echo $team;?></p>
 				<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -216,7 +224,7 @@
 													FROM atbatdetails
 													WHERE atbatdetails.pid = '$id' AND 
 													atbatdetails.batterteam ='$team'
-													ORDER BY atbatdetails.gdate DESC ")
+													ORDER BY atbatdetails.gdate DESC");
 					?>
 							<!-- PHP CODE TO FETCH DATA FROM ROWS-->
 					<?php 
