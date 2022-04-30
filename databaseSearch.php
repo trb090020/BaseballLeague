5eca0f158f7c147a7fb008e3b243927f5f6b9bbf
Andrew the XII <54558003+youandrew@users.noreply.github.com>
2022-04-29 20:40:08 -0500

    Update databaseSearch.php

--- databaseSearch.php
+++ databaseSearch.php
@@ -1,16 +1,20 @@
 <?php
  
 	$dataPoints = array();
-	//$dataPoints2 = array();
+	$dataPoints2 = array();
 
-    //$result = pg_query($PGDB, "SELECT DISTINCT DATE(gdate) FROM AB WHERE PID='$id' ORDER by gdate DESC");
+ 
 	$result = pg_query($PGDB, "SELECT DISTINCT DATE(gdate) as bdate , AVG(strikeCount) as avgStrike FROM ( SELECT gdate , sum(strikes) AS strikeCount FROM AB GROUP BY gdate) AS foo GROUP BY gdate   ");
-	//$result2 = pg_query($PGDB, "SELECT DISTINCT DATE(gdate), AVG(strikeCount) as avgStrike FROM ( SELECT gdate, sum(strikes) AS strikeCount FROM AB GROUP BY gdate) GROUP BY gdate ");
+	$result2 = pg_query($PGDB, "SELECT DISTINCT DATE(gdate) as bdate , AVG(ballCount) as avgBall FROM ( SELECT gdate , sum(balls) AS ballCount FROM AB GROUP BY gdate) AS foo GROUP BY gdate   ");
 	
     foreach($result as $row){
+	console.log($row["bdate"]);
+	console.log($row["avgStrike"]);
         array_push($dataPoints, array("x"=> $row["bdate"], "y"=> $row["avgStrike"]));
     }
-	
+    foreach($result2 as $row){
+        array_push($dataPoints, array("x"=> $row["bdate"], "y"=> $row["avgBall"]));
+    }
 
 
 	
@@ -25,18 +29,33 @@
 		
 		var chart = new CanvasJS.Chart("graphContainer", {
 			title: {
-				text: "Date"
+				text: "Pitcher"
 			},
 			axisY: {
 				title: "Strikes"
+			},
+			axisX: {
+				title: "Date"
 			},
 			data: [{
 				type: "line",
 				dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
 			}]
 		});
+		var chart2 = new CanvasJS.Chart("graphContainer2", {
+			title: {
+				text: "Batter"
+			},
+			axisY: {
+				title: "balls"
+			},
+			data: [{
+				type: "line",
+				dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+			}]
+		});	       
 		chart.render();
-		
+		chart2.render();
 		}
 		</script>
     </head>
@@ -50,6 +69,7 @@
 
 			<p class="text"><b>You searched for: </b><?php echo str_repeat('&nbsp;', 2); echo $number; echo str_repeat('&nbsp;', 2); echo $fname; ?></p>
 			<div id="graphContainer" style="height: 400px; width: 100%;"></div>
+			<div id="graphContainer2" style="height: 400px; width: 100%;"></div>
 			<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 			<?php
 				ini_set('display_errors', 'on');
