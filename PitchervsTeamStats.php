a2a71443a058c918a2df09ef3f50b15ef0ab4b9c
JosiahJohns <57999307+JosiahJohns@users.noreply.github.com>
2022-04-29 20:11:48 -0500

    Update PitchervsTeamStats.php

--- PitchervsTeamStats.php
+++ PitchervsTeamStats.php
@@ -6,7 +6,6 @@
     </head>
 	<body>
 		<h1><b>Pitcher vs. Team Statistics</b></h1><br>
-		<div class="login">
 			<?php
 				$id = $_GET['id'];
 				$team = $_GET['team'];
@@ -20,21 +19,21 @@
 							catch (PDOException $e) 
 							{die($e->getMessage());}
 							
-							$pitcherfname = pg_query($PGDB, "SELECT  first_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
-							$pitcherlname = pg_query($PGDB, "SELECT  last_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
+							$pitcherfname = pg_query($PGDB, "SELECT first_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
+							$pitcherlname = pg_query($PGDB, "SELECT last_name FROM public.allpitchers WHERE public.allpitchers.id = '$id'");
 							
-							$result = pg_query($PGDB, "SELECT 
-											ROUND(AVG(strikes),2) AS Strikes,
-											ROUND(AVG(balls),2) AS Balls,
-											ROUND(AVG(bases),2) AS Bases,
-											COUNT(*) AS AtBatCount,
-											COUNT(case when abresult='H' then 1 end) as Hits,
-											COUNT(case when abresult='O' then 1 end) as StrikeOuts,
-											ROUND((COUNT(case when abresult='H' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS percentHits,
-											ROUND((COUNT(case when abresult='O' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS PercentStrikeOuts
-											FROM public.atbatdetails
-											WHERE public.atbatdetails.pid = '$id'
-											AND public.atbatdetails.batterteam = '$team' ");
+							$result1 = pg_query($PGDB, "SELECT 
+													ROUND(AVG(strikes),2) AS Strikes,
+													ROUND(AVG(balls),2) AS Balls,
+													ROUND(AVG(bases),2) AS Bases,
+													COUNT(*) AS AtBatCount,
+													COUNT(case when abresult='H' then 1 end) as Hits,
+													COUNT(case when abresult='O' then 1 end) as StrikeOuts,
+													ROUND((COUNT(case when abresult='H' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS percentHits,
+													ROUND((COUNT(case when abresult='O' then 1 end)*100.0)/(COUNT(*)*1.0),2) AS PercentStrikeOuts
+													FROM public.atbatdetails
+													WHERE public.atbatdetails.pid = '$id'
+													AND public.atbatdetails.batterteam = '$team' ");
 											
 							$result2 = pg_query($PGDB,  "SELECT 
 													ROUND(AVG(strikes),2) AS Strikes,
@@ -54,7 +53,7 @@
 													WHERE public.atbatdetails.pid = '$id' AND public.atbatdetails.batterteam ='$team' 
 													ORDER BY public.atbatdetails.gdate 
 													LIMIT 1 
-													OFFSET 9");
+													OFFSET 9)");
 													
 							$result3 = pg_query($PGDB,  "SELECT 
 													ROUND(AVG(strikes),2) AS Strikes,
@@ -74,7 +73,7 @@
 													WHERE public.atbatdetails.pid = '$id' AND public.atbatdetails.batterteam ='$team' 
 													ORDER BY public.atbatdetails.gdate 
 													LIMIT 1 
-													OFFSET 4");
+													OFFSET 4)");
 													
 						$result4 = pg_query($PGDB,  "SELECT 
 													public.atbatdetails.gdate as d,
@@ -93,9 +92,11 @@
 													ORDER BY public.atbatdetails.gdate DESC");
 					
 			?>
-			<hr size="1" width="100%" color="black">
+			<p class="text"><b>Statistics: <?php echo $pitcherfname;?> <?php echo $pitcherlname;?> vs. <?php echo $team;?> </b></p>
 
-			<p class="text"><b>All-Time Statistics: </b><?php echo $pitcherfname." ".$pitcherlname." ";?><b>vs. </b> <?php echo $team;?></p>
+			<hr size="1" width="100%" color="black">
+			<h2><b>All- Time</b></h2><br>	
+			
 			<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -110,18 +111,18 @@
 
 							<!-- PHP CODE TO FETCH DATA FROM ROWS-->
 					<?php 
-							while ($rows = pg_fetch_assoc($result))
+							while ($rows1 = pg_fetch_assoc($result1))
 							{
 					?>
 						<tr>
 							<!--FETCHING DATA FROM EACH 
 								ROW OF EVERY COLUMN-->
-							<td><?php echo $rows['strikes']; ?></td>
-							<td><?php echo $rows['balls']; ?></td>
-							<td><?php echo $rows['bases']; ?></td>
-							<td><?php echo $rows['atbatcount']; ?></td>
-							<td><?php echo $rows['percentHits']; ?></td>
-							<td><?php echo $rows['PercentStrikeOuts']; ?></td>
+							<td><?php echo $rows1['strikes']; ?></td>
+							<td><?php echo $rows1['balls']; ?></td>
+							<td><?php echo $rows1['bases']; ?></td>
+							<td><?php echo $rows1['atbatcount']; ?></td>
+							<td><?php echo $rows1['percentHits']; ?></td>
+							<td><?php echo $rows1['PercentStrikeOuts']; ?></td>
 						</tr>
 					<?php
 							}
@@ -131,8 +132,7 @@
 			</section>
 				
 			<hr size="1" width="100%" color="black">
-			<h2><b>Pitcher vs. Team Statistics (Last 10 Games)</b></h2><br>	
-			<p class="text"><b>Statistics For Last 10 Games:</b><?php echo $pitcherfname." ".$pitcherlname." ";?><b>vs. </b> <?php echo $team;?></p>
+			<h2><b>Last 10 Games</b></h2><br>	
 			<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -153,12 +153,12 @@
 						<tr>
 							<!--FETCHING DATA FROM EACH 
 								ROW OF EVERY COLUMN-->
-							<td><?php echo $rows['strikes']; ?></td>
-							<td><?php echo $rows['balls']; ?></td>
-							<td><?php echo $rows['bases']; ?></td>
-							<td><?php echo $rows['atbatcount']; ?></td>
-							<td><?php echo $rows['percentHits']; ?></td>
-							<td><?php echo $rows['PercentStrikeOuts']; ?></td>
+							<td><?php echo $rows2['strikes']; ?></td>
+							<td><?php echo $rows2['balls']; ?></td>
+							<td><?php echo $rows2['bases']; ?></td>
+							<td><?php echo $rows2['atbatcount']; ?></td>
+							<td><?php echo $rows2['percentHits']; ?></td>
+							<td><?php echo $rows2['PercentStrikeOuts']; ?></td>
 						</tr>
 					<?php
 							}
@@ -168,8 +168,7 @@
 				</section>
 				
 				<hr size="1" width="100%" color="black">
-				<h2><b>Pitcher vs. Team Statistics (Last 5 Games)</b></h2><br>
-				<p class="text"><b>Statistics For Last 5 Games:</b><?php echo $pitcherfname." ".$pitcherlname." ";?><b>vs. </b> <?php echo $team;?></p>
+				<h2><b>Last 5 Games</b></h2><br>
 				<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -190,12 +189,12 @@
 						<tr>
 							<!--FETCHING DATA FROM EACH 
 								ROW OF EVERY COLUMN-->
-							<td><?php echo $rows['strikes']; ?></td>
-							<td><?php echo $rows['balls']; ?></td>
-							<td><?php echo $rows['bases']; ?></td>
-							<td><?php echo $rows['atbatcount']; ?></td>
-							<td><?php echo $rows['percentHits']; ?></td>
-							<td><?php echo $rows['PercentStrikeOuts']; ?></td>
+							<td><?php echo $rows3['strikes']; ?></td>
+							<td><?php echo $rows3['balls']; ?></td>
+							<td><?php echo $rows3['bases']; ?></td>
+							<td><?php echo $rows3['atbatcount']; ?></td>
+							<td><?php echo $rows3['percentHits']; ?></td>
+							<td><?php echo $rows3['PercentStrikeOuts']; ?></td>
 						</tr>
 					<?php
 							}
@@ -205,8 +204,8 @@
 				</section>
 				
 				<hr size="1" width="100%" color="black">
-				<h2><b>Pitcher vs. Team Statistics (All At Bat Records)</b></h2><br>
-				<p class="text"><b>All At Bat Records:</b><?php echo $pitcherfname." ".$pitcherlname." ";?><b>vs. </b> <?php echo $team;?></p>
+				<h2><b>All At Bat Records</b></h2><br>
+				
 				<section>
 					<!-- TABLE CONSTRUCTION-->
 					<table class="center">
@@ -231,16 +230,16 @@
 						<tr>
 							<!--FETCHING DATA FROM EACH 
 								ROW OF EVERY COLUMN-->
-							<td><?php echo $rows['d']; ?></td>
-							<td><?php echo $rows['l']; ?></td>
-							<td><?php echo $rows['bid']; ?></td>
-							<td><?php echo $rows['f']; ?></td>
-							<td><?php echo $rows['lst']; ?></td>
-							<td><?php echo $rows['tm']; ?></td>
-							<td><?php echo $rows['strikes']; ?></td>
-							<td><?php echo $rows['balls']; ?></td>
-							<td><?php echo $rows['bases']; ?></td>
-							<td><?php echo $rows['abresult']; ?></td>
+							<td><?php echo $rows4['d']; ?></td>
+							<td><?php echo $rows4['l']; ?></td>
+							<td><?php echo $rows4['bid']; ?></td>
+							<td><?php echo $rows4['f']; ?></td>
+							<td><?php echo $rows4['lst']; ?></td>
+							<td><?php echo $rows4['tm']; ?></td>
+							<td><?php echo $rows4['strikes']; ?></td>
+							<td><?php echo $rows4['balls']; ?></td>
+							<td><?php echo $rows4['bases']; ?></td>
+							<td><?php echo $rows4['abresult']; ?></td>
 						</tr>
 					<?php
 							}
